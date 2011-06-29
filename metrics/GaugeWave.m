1995a2458da668df0b3cb9eacda758b0e4da5494
Barry Wardell <barry.wardell@gmail.com>
2011-06-29 11:46:59 +0200

    Add GaugeWave metric.

---
+++ metrics/GaugeWave.m
@@ -0,0 +1,14 @@
+(* ::Package:: *)
+
+{
+  "Name" -> "GaugeWave",
+  "Description" -> "4D flat spacetime in Cartesian gauge wave coordinates",
+  "Dimensions" -> 4,
+  "Coordinates" -> {t, x, y, z}, 
+  "Metric" -> {{-Sqrt[(1-amp Sin[2 \[Pi] (x-t) / period])], 0, 0, 0},
+               {0, 1 - amp Sin[2 \[Pi] (x-t) / period], 0, 0},
+               {0, 0, 1, 0},
+               {0, 0, 0, 1}},
+  "SignDet" -> -1,
+  "Parameters" -> {amp, period}
+}
