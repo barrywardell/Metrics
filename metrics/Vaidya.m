7d134a224bd2400772e99e52de47c082b0938772
Barry Wardell <barry.wardell@gmail.com>
2011-11-27 21:25:52 +0000

    Some changes from Erik: * Improve some comments. * Make the definition of the mass function clearer.

--- metrics/Vaidya.m
+++ metrics/Vaidya.m
@@ -1,16 +1,12 @@
 (* ::Package:: *)
 
-(* The Vaidya spacetimes are parametrized by a mass function m(v). Some common
-   choices are:
-    Minkowski:              m = 0
-    Schwarzschild:          m = M
-    Simple Vaidya:          m = M + dM v
-    More complex Vaidya:    m = M (1 + Tanh[v dM / M]^2)
-   Here, we implement the more complex Vaidya type. This can be changed by
-   modifying the shorthand below.
+(* The Vaidya spacetimes are parametrized by a mass function m(v)
+   that can be chosen freely. Here, we implement
+      m(v) = M (1 + Tanh[v dM / M]^2)
+   This can be changed by modifying the shorthand below.
  *)
 
-Module[{rx, thetax, phix, theta, phi, vu, rv, u, v, gv, CX, J, gx},
+Module[{rx, thetax, phix, v, u, theta, phi, vu, rv, gv, CX, J, gx},
   (* r in terms of Cartesian coordinates *)
   rx = Sqrt[x^2+y^2+z^2];
   thetax = ArcCos[z / rx];
@@ -24,7 +20,7 @@ Module[{rx, thetax, phix, theta, phi, vu, rv, u, v, gv, CX, J, gx},
   gv = {{ -(1-2 m[t,x,y,z]/rv), 1, 0, 0}, {1, 0, 0, 0},
         {0, 0, rv^2, 0}, {0, 0, 0, rv^2 Sin[theta]^2}};
 
-  (* Coordinate transformation , Vaidya -> Cartesian*)
+  (* Coordinate transformation, Vaidya -> Cartesian*)
   CX = {v->vu[[1]], u->vu[[2]], theta->vu[[3]], phi->vu[[4]]};
 
   (* Vaidya metric, in Vaidya coordinates, as function of Cartesian
@@ -35,16 +31,17 @@ Module[{rx, thetax, phix, theta, phi, vu, rv, u, v, gv, CX, J, gx},
   J = Simplify[Table[D[vu, X], {X, {t, x, y, z}}]];
 
   (* Convert metric to Cartesian coordinates *)
-  gx = Simplify[J.gv.Transpose[J]] /. {(rx^2)^n_ :>  r[x,y,z]^(2n)};
+  gx = Simplify[J.gv.Transpose[J]] /. {(rx^2)^n_ :> r[x,y,z]^(2n)};
 
   {
     "Name" -> "Vaidya",
-    "Description" -> "A Vaidya spacetime with mass function M (1 + Tanh[v dM / M]^2)",
+    "Description" -> "A Vaidya spacetime with mass function m(v) = M (1 + Tanh[v dM / M]^2)",
     "Dimensions" -> 4,
     "Coordinates" -> {t, x, y, z},
     "Parameters" -> {M, dM},
     "Metric" -> gx,
     "SignDet" -> -1,
-    "Shorthands" -> {r[x,y,z] -> Sqrt[x^2+y^2+z^2], m[t,x,y,z] -> M (1 + Tanh[vu[[1]] dM / M]^2)}
+    "Shorthands" -> {r[x,y,z] -> Sqrt[x^2+y^2+z^2],
+                     m[t,x,y,z] -> M (1 + Tanh[v dM / M]^2)} /. CX
   }
 ]
